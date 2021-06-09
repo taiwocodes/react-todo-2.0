d1514962c847aa384b6b94bb137ef431a9fbd2df
Taiwo <famaktaiwo7@gmail.com>
2021-06-10 00:58:32 +0100

    updated data in sc/index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './app';
 
 const DATA = [
-  { id: "todo-0", name: "Eat", completed: true },
-  { id: "todo-1", name: "Sleep", completed: false },
-  { id: "todo-2", name: "Repeat", completed: false }
+	{ id: 'todo-0', name: 'Pick up laundry', completed: true },
+	{ id: 'todo-1', name: 'Get fruits for Kevin', completed: false },
+	{ id: 'todo-2', name: 'Do grocery shopping', completed: false },
 ];
 
-ReactDOM.render(<App tasks={DATA} />, document.getElementById("root"));
+ReactDOM.render(<App tasks={DATA} />, document.getElementById('root'));
